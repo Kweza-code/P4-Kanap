b95532ab11ca731829e853c771cf3924e2f5ffac
innocent75 <99754303+innocent75@users.noreply.github.com>
2022-06-14 13:43:47 +0200

    2 em tentative

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,32 +1,13 @@
-main();
-
-async function main() {
-    const articles = await getArticles()
-    for (articles of articles) {
-        displayArticles(article)
+fetch("http://localhost:3000/api/products")
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
     }
-    
-}
+  })
+  .then(function(value) {
+    console.log(value);
+  })
+  .catch(function(err) {
+    // Une erreur est survenue
+  });
 
-function getArticles() {
-   return fetch("http://localhost:3000/api/products")
-    .then(function(httpBodyResponse) {
-    return httpBodyResponse.json()
-    })
-    .then(function(articles){
-        return articles
-    })
-    .catch(function(error) {
-        alert(error)
-    })
-}
-
-function displayArticles(article) {
-   document.getElementbyId("items").inner.HTML += `   <a href="./product.html?id=42">
-   <article>
-     <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
-     <h3 class="productName">Kanap name1</h3>
-     <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-   </article>
- </a> -->`
-}
